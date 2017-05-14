2fd3c1c680e43a63bcb99912c9f41eab727316b2
Ian Nelson <ianwalshnelson@gmail.com>
2017-05-14 19:38:46 -0400

    Add YQL query to make jsonp request on XML data

--- js/great-schools.js
+++ js/great-schools.js
@@ -3,36 +3,61 @@ function loadGs(item) {
 
     var url = "http://api.greatschools.org/schools/NY/";
     var schoolId = $(item).attr("id");
+    var schoolName = $(item).text();
     var gsKey = "?key=6i8inihoymrgg9dvcmwzfqxx";
     url += schoolId;
     url += gsKey;
 
-    var request = $.get(url)
 
-    request.success(function(responseText) {
-        var $gsLink = $('#gsLink');
-        var $gsRating = $('#gsRating');
-        var $name = $(item).text();
-        var rating = responseText.getElementsByTagName("gsRating")[0].childNodes[0].nodeValue;
-        var gsUrl = responseText.getElementsByTagName("overviewLink")[0].childNodes[0].nodeValue;
-        $gsRating.text('GreatSchools Rating: ' + rating + ' / 10');
-        $gsLink.text(name + ' GreatSchools Profile');
-        $gsLink.attr('href', gsUrl);
-    });
+    // Generate YQL Query
+    var yqlURL = [
+        "http://query.yahooapis.com/v1/public/yql",
+        "?q=" + encodeURIComponent("select * from xml where url='" + url + "'"),
+        "&format=xml&callback=?"
+    ].join("");
 
-    request.error(function(jqXHR, textStatus, errorThrown) {
-        if (textStatus == 'timeout')
-            console.log('The server is not responding');
-
-        if (textStatus == 'error')
-            console.log(errorThrown);
-
+    // Set timout for YQL result
+    var yqlRequestTimeout = setTimeout (function(){
+        console.log('Did not received YQL response within 3 seconds while searching for ' + schoolName);
         var $gsErr = $('#gsErr');
-        $gsErr.text('Unable to reach GreatSchools.org')
+        $gsErr.text("Failed to connect to GreatSchools.org");
+    }, 3000);
+
+    // Ajax request of YQL results
+    $.ajax({
+        url: yqlURL,
+        dataType: "jsonp",
+        success: function(response){
+            console.log(response);
+            xmlContent = $(response.results[0]);
+            var rating = $(xmlContent).find("gsRating").text();
+            var gsUrl = $(xmlContent).find("overviewLink").text();
+
+            // If GreatSchols API request was not successful
+            if ( rating == '' || gsUrl == '' ) {
+
+                console.log('Returned error from YQL query on GreatSchools.org request on ' + schoolName);
+                var $gsErr = $('#gsErr');
+                $gsErr.text("Failed to connect to GreatSchools.org");
+                clearTimeout(yqlRequestTimeout);
+
+            // If GreatSchools request successful
+            } else {
+
+                console.log('Successfully returned GreatSchools.org data from YQL for ' + schoolName);
+                var $gsLink = $('#gsLink');
+                var $gsRating = $('#gsRating');
+                var $gsErr = $('#gsErr');
+
+                $gsRating.text('GreatSchools Rating: ' + rating + ' / 10');
+                $gsLink.text(name + ' GreatSchools Profile');
+                $gsLink.attr('href', gsUrl);
+
+
+                $gsErr.text('');
+                clearTimeout(yqlRequestTimeout);
+            }
+
+        }
     });
-
-};
-
-// $( document ).ready(loadGs());
-
-
+}
